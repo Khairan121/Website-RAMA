ad2024e4212cb5699137c077daee0e5aec6511d2
Khairan121 <100358266+Khairan121@users.noreply.github.com>
2025-10-04 14:58:16 +0700

    ganti warna, ubah layout sedikit, kasih animasi

--- js/script.js
+++ js/script.js
@@ -29,12 +29,26 @@ const menuNav = document.getElementById('menuNav');
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 50) {
-        navbar.classList.add('bg-[#c2cca6]/70', 'backdrop-blur-md');
-        logo.classList.add('text-[#2E361C]');
-        menuNav.classList.add('text-[#2E361C]');
+        navbar.classList.add('bg-[#152238]/70', 'backdrop-blur-md');
+        logo.classList.add('text-[#fadbb9]');
+        menuNav.classList.add('text-[#fadbb9]');
     } else {
-        navbar.classList.remove('bg-[#c2cca6]/70', 'backdrop-blur-md');
-        logo.classList.remove('text-[#2E361C]');
-        menuNav.classList.remove('text-[#2E361C]');
+        navbar.classList.remove('bg-[#152238]/70', 'backdrop-blur-md');
+        logo.classList.remove('text-[#fadbb9]');
+        menuNav.classList.remove('text-[#fadbb9]');
     }
 });
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("opacity-100", "translate-x-0");
+            entry.target.classList.remove("opacity-0", "-translate-x-12");
+        } else {
+            entry.target.classList.remove("opacity-100", "translate-x-0");
+            entry.target.classList.add("opacity-0", "-translate-x-12");
+        }
+    });
+}, { threshold: 0.1 });
+
+document.querySelectorAll(".box").forEach((el) => observer.observe(el));
